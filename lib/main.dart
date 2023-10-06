f56ef3a40476ec45cf665cf7fa4111338b038c90
Bqwer_Qwert <bqwer34@gmail.com>
2023-10-06 20:49:51 +0000

    my app

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(home: all()));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class all extends StatefulWidget {
+  const all({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<all> createState() => _allState();
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class _allState extends State<all> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+          backgroundColor: Colors.cyan,
+          title: Center(
+            child: Text(
+              'the name',
+              style: TextStyle(fontSize: 35),
+            ),
+          )),
+      body: SafeArea(
+          child: Center(
+              child: Container(
+                  width: 300,
+                  height: 79,
+                  color: Colors.green,
+                  child: Center(
+                      child: Text(
+                    'my home',
+                    style: TextStyle(
+                        color: Colors.pink,
+                        fontSize: 50,
+                        fontWeight: FontWeight.w800,
+                        fontStyle: FontStyle.italic),
+                  ))))),
     );
   }
 }

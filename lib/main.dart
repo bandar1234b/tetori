43d8c1e25d16e9727f6d7261292baf4d626501d7
Bqwer_Qwert <bqwer34@gmail.com>
2023-10-07 11:05:43 +0000

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: all()));
+void main() => runApp(MaterialApp(home: icon())); //all()
 
 class all extends StatefulWidget {
   const all({Key? key}) : super(key: key);
@@ -28,14 +28,38 @@ class _allState extends State<all> {
                   height: 79,
                   color: Colors.green,
                   child: Center(
-                      child: Text(
-                    'my home',
-                    style: TextStyle(
-                        color: Colors.pink,
-                        fontSize: 50,
-                        fontWeight: FontWeight.w800,
-                        fontStyle: FontStyle.italic),
-                  ))))),
+                      child: Padding(
+                          padding: EdgeInsets.only(bottom: 20),
+                          child: Text(
+                            'my home',
+                            style: TextStyle(
+                                color: Colors.pink,
+                                fontSize: 50,
+                                fontWeight: FontWeight.w800,
+                                fontStyle: FontStyle.italic),
+                          )))))),
     );
+  }
+}
+
+class icon extends StatefulWidget {
+  const icon({Key? key}) : super(key: key);
+
+  @override
+  State<icon> createState() => _iconState();
+}
+
+class _iconState extends State<icon> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            body: Center(
+      child: Icon(
+        Icons.home,
+        size: 100,
+        color: Colors.purple,
+      ),
+    )));
   }
 }
